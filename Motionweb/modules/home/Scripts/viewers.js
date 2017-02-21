e058af0666171109b0b86038ae23df1253e1f79a
john <johnjohnson45@live.com>
2017-02-20 22:18:59 -0500

    Change

--- Motionweb/modules/home/Scripts/viewers.js
+++ Motionweb/modules/home/Scripts/viewers.js
@@ -2,13 +2,13 @@
 angular.module('viewers', [])
 .controller('viewersController', ['$scope','$location', function($scope,$location){
 	$scope.viewers = [
-	{firstName:'Test',lastName:'Testing',email:'Testing@live.com'},
-	{firstName:'Test',lastName:'Testing',email:'Testing@live.com'},
-	{firstName:'Test',lastName:'Testing',email:'Testing@live.com'},
-	{firstName:'Test',lastName:'Testing',email:'Testing@live.com'},
-	{firstName:'Test',lastName:'Testing',email:'Testing@live.com'},
-	{firstName:'Test',lastName:'Testing',email:'Testing@live.com'},
-	{firstName:'Test',lastName:'Testing',email:'Testing@live.com'}
+	{firstName:'Test1',lastName:'Testing',email:'Testing@live.com'},
+	{firstName:'Test2',lastName:'Testing',email:'Testing@live.com'},
+	{firstName:'Test3',lastName:'Testing',email:'Testing@live.com'},
+	{firstName:'Test4',lastName:'Testing',email:'Testing@live.com'},
+	{firstName:'Test5',lastName:'Testing',email:'Testing@live.com'},
+	{firstName:'Test6',lastName:'Testing',email:'Testing@live.com'},
+	{firstName:'Test7',lastName:'Testing',email:'Testing@live.com'}
 	];
 
 	//Replace with api call once created
