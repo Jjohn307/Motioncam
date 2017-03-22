ce38528a72c413ba71d825fda2a4c3a25c847682
alexkandres <alexkandres@live.com>
2017-03-22 19:32:17 +0000

    Edit scripts

--- scripts/downloadDriver.sh
+++ scripts/downloadDriver.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
-#loads module on start if placed in ~/.bashrc
-#probably can create script to install once
-sudo modprobe bcm2835-v4l2
+#load the module
+#sudo modprobe bcm2835-v4l2
+
+cd /etc
+
+#intalling
+#if module exist then do nothing, otherwise insert module
+if [ $(grep -i bcm2835-v4l2 modules) ];
+then
+	echo "module exist"
+else
+	echo bcm2835-v4l2 >> modules
+	echo "not exist, please insert"
+fi
+
--- scripts/mjpgStream.sh
+++ scripts/mjpgStream.sh
@@ -1,8 +1,24 @@
 #! /bin/bash
 
+#download zipped mjpeg
+#unzip it
+
+#install dependencies for mjpg
+#probably can include dependencies for python code
+#sudo apt-get install libjpeg8-dev imagemagick libv4l-dev
+
+wget http://ec2-35-162-159-206.us-west-2.compute.amazonaws.com/downloads/mjpg-streamer.tar.gz
+
+tar -xvzf mjpg-streamer.tar.gz
+
+#will work after libv4l-dev is installed
+sudo ln -s /usr/include/libv4l1-videodev.h /usr/include/linux/videodev.h
+cd mjpg-streamer/
+make
+
 #assume that mjpg-strema is installed in home directory
-cd ~/mjpg-streamer
+#cd ~/mjpg-streamer
 
 #run Streamer
-sudo ./mjpg_streamer -i "./input_uvc.so -f 5 -r 640x320 -n -y -q 20" -o "./output_http.so -w ./www -p 80"
+#sudo ./mjpg_streamer -i "./input_uvc.so -f 5 -r 640x320 -n -y -q 20" -o "./output_http.so -w ./www -p 80"
 
