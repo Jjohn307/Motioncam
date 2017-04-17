fb6a119874f35d2d31c1e352a7d8192e1971c9c1
alexkandres <alexkandres@live.com>
2017-04-17 19:05:06 -0400

    Add libav-tools to install

--- Motionweb/downloadPython.php
+++ Motionweb/downloadPython.php
@@ -276,7 +276,7 @@ sudo apt-get -y install python-opencv
 
 #mjpeg streamer dependencies
 #install mjpeg
-sudo apt-get -y install libjpeg8-dev imagemagick libv4l-dev
+sudo apt-get -y install libjpeg8-dev imagemagick libv4l-dev libav-tools
 cd /home/pi
 wget http://ec2-35-162-159-206.us-west-2.compute.amazonaws.com/downloads/mjpg-streamer.tar.gz
 tar -xvzf mjpg-streamer.tar.gz
