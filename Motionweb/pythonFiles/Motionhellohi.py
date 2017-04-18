7674ecb00978f7707cded9c033849eddbca18283
alexkandres <alexkandres@live.com>
2017-04-18 11:52:43 -0400

    Add open file notify.txt

--- Motionweb/pythonFiles/Motionhellohi.py
+++ Motionweb/pythonFiles/Motionhellohi.py
@@ -1,22 +1,4 @@
-<?php 
-	
-	if(isset($_GET["user"]))
-		$user = $_GET["user"];
-	else
-		echo "User not set";
-
-	if(isset($_GET["cameraName"])){
-        $cameraName = $_GET["cameraName"];
-    }
-    else{
-        $cameraName = "CameraName";
-    }
-    $file = "pythonFiles/Motion" . $user . $cameraName.".py";
-    $file2 = "pythonFiles/script" . $user . $cameraName.".sh";
-
-	//TODO code that user will download (Could be static for now)
-	$pythonCode =
-        "import numpy as np
+import numpy as np
 import imutils
 import datetime 
 import cv2
@@ -43,8 +25,8 @@ videoQueue = Queue.Queue()
 runningconversionthread = False
 AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
-username = '".$user."'
-cameraname = '".$cameraName."'
+username = 'hello'
+cameraname = 'hi'
 
 
 
@@ -57,17 +39,17 @@ def sendnotificationTos3():
         s3bucket =  connection.get_bucket('sketchflow')
         k = Key(s3bucket)
         k.key = username+'/'+'notification'+sendtime+'.txt'
-        k.set_contents_from_filename(\"notify.txt\")
-        print \"user notified of suspicious activity\"
+        k.set_contents_from_filename("notify.txt")
+        print "user notified of suspicious activity"
         
 
 #sending recorded video to amazon server
 def sendTos3(Filename):
         now   = datetime.datetime.now()
         sendtime=now.strftime('%m%d%Y%H%M%S')
-        FilenameMp4 = Filename+\".mp4\"
-        thumbnail = Filename+\".jpg\"
-        FilenameAvi = Filename + \".avi\"
+        FilenameMp4 = Filename+".mp4"
+        thumbnail = Filename+".jpg"
+        FilenameAvi = Filename + ".avi"
         #need access key Id and secret Access Key 
         s3bucket_name = AWS_ACCESS_KEY_ID.lower() +'-uploads'
         connection = boto.connect_s3(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY)
@@ -77,10 +59,10 @@ def sendTos3(Filename):
         k.set_contents_from_filename(thumbnail)
         k.key = username+'/'+cameraname+'/'+sendtime+'.mp4'
         k.set_contents_from_filename(FilenameMp4)
-        print (\"sent\")
-        subprocess.call([\"rm\",FilenameMp4])
-        subprocess.call([\"rm\",FilenameAvi])
-        subprocess.call([\"rm\",thumbnail])
+        print ("sent")
+        subprocess.call(["rm",FilenameMp4])
+        subprocess.call(["rm",FilenameAvi])
+        subprocess.call(["rm",thumbnail])
         
 
 #convert video from avi to mp4. Conversion is used since HTML5 is not avi friendly.
@@ -88,10 +70,10 @@ def sendTos3(Filename):
 def convertTomp4():
        while not videoQueue.empty():
                 video  = videoQueue.get();
-                print\"now conveting video\"
-                subprocess.call([\"avconv\",\"-i\",video+\".avi\",'-c:v','libx264',video+\".mp4\"])
+                print"now conveting video"
+                subprocess.call(["avconv","-i",video+".avi",'-c:v','libx264',video+".mp4"])
                 time.sleep(20)
-                print\"video converted\"
+                print"video converted"
                 sendTos3(video)
        runningconversionthread = False
                 
@@ -99,8 +81,8 @@ def convertTomp4():
 def Recording(vs):
     now = datetime.datetime.now();
     createdTime=now.strftime('%m%d%Y%H%M%S')
-    filename =  createdTime+\".avi\"
-    #filename2 = \"Hello_test3.mp4\"
+    filename =  createdTime+".avi"
+    #filename2 = "Hello_test3.mp4"
     thumbnailfile = createdTime+'.jpg'
     #depending on opencv version use recommended way to obtain four_cc
     #fourcc = cv2.VideoWriter_fourcc(*'MP4V')
@@ -114,7 +96,7 @@ def Recording(vs):
         if thumbnail:
                 cv2.imwrite(thumbnailfile,frame)
                 thumbnail = False
-        print (\"Recording\")
+        print ("Recording")
         writer.write(frame)
         
     
@@ -156,22 +138,21 @@ def motiondetection(gray,frame,prev):
         (x, y, w, h) = cv2.boundingRect(c)
         motion = True
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    cv2.imshow(\"Security Feed\", frame)
+    cv2.imshow("Security Feed", frame)
     key = cv2.waitKey(1)
     return motion
 
            
    
 
-if __name__ == \"__main__\":
+if __name__ == "__main__":
     
-        notifyFile = open(\"notify.txt\", \"wb\")
-        facialCascade = cv2.CascadeClassifier(\"haarcascades/haarcascade_frontalface_default.xml\")
+        facialCascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
         prev = None
         createdtime = None
         cnt = 0
         time_end = None
-        vs = WebcamVideoStream(src = \"http://localhost/?action=stream/frame.mjpg\").start()
+        vs = WebcamVideoStream(src = "http://localhost/?action=stream/frame.mjpg").start()
         fps =  FPS().start()
         while True:
 
@@ -196,7 +177,7 @@ if __name__ == \"__main__\":
                 face = facedetection(gray,frame1)
                 if face:
 
-                    print \"face detected\"
+                    print "face detected"
                     Recording(camera)
                     
                 elif motion:
@@ -204,7 +185,7 @@ if __name__ == \"__main__\":
                         if time_end is None:
                                 time_end = time.time() + 2
                         cnt = cnt+1
-                        print \"cnt\",cnt
+                        print "cnt",cnt
                         
                 if((time_end <= time.time() or cnt >= 5) and time_end != None and not recording ):
 
@@ -214,14 +195,14 @@ if __name__ == \"__main__\":
                                 lengthofrecordingVideo =  time.time()+recordingtime
                                 now = datetime.datetime.now();
                                 createdTime=now.strftime('%m%d%Y%H%M%S')
-                                filename =  createdTime+\".avi\"
+                                filename =  createdTime+".avi"
                                 thumbnailfile = createdTime+'.jpg'
                                 fourcc = cv2.cv.CV_FOURCC(*'XVID')
                                 writer = cv2.VideoWriter(filename,fourcc,15.00,(640,480))
                                 cv2.imwrite(thumbnailfile,frame)
-                                print \"There is suffient amount of evidence\"
+                                print "There is suffient amount of evidence"
                         else:
-                                print \"There is not suffient amount of evidence proving that motion detected was suspicous\"
+                                print "There is not suffient amount of evidence proving that motion detected was suspicous"
                         cnt = 0
                         time_end = None
                         
@@ -230,7 +211,7 @@ if __name__ == \"__main__\":
                 if recording:
                         if lengthofrecordingVideo >= time.time():
                                 writer.write(frame)
-                                print \" recordinng video\"
+                                print " recordinng video"
                         else:
                                 writer.release()
                                 recording = False
@@ -247,66 +228,8 @@ if __name__ == \"__main__\":
                         conversionanduploadThread.start()
                         
             else:
-                print \"error when reading from camera\"
-                notifyFile.close()
+                print "error when reading from camera"
                 break
             key = cv2.waitKey(1) & 0xFF
-            if key == ord(\"q\"):
-                notifyFile.close()
-                break";
-
-	$shellCode =
-        "#! /bin/bash
-
-#load the module
-sudo modprobe bcm2835-v4l2
-
-cd /etc
-
-#intalling
-#if module exist then do nothing, otherwise insert module
-if [ $(grep -i bcm2835-v4l2 modules) ];
-then
-	echo \"module exist\"
-else
-	echo bcm2835-v4l2 >> modules
-	echo \"not exist, please insert\"
-fi
-
-#python dependencies
-pip install boto numpy imutils
-sudo apt-get -y install python-opencv
-
-#mjpeg streamer dependencies
-#install mjpeg
-sudo apt-get -y install libjpeg8-dev imagemagick libv4l-dev libav-tools
-cd /home/pi
-wget http://ec2-35-162-159-206.us-west-2.compute.amazonaws.com/downloads/mjpg-streamer.tar.gz
-tar -xvzf mjpg-streamer.tar.gz
-
-#will work after libv4l-dev is installed
-sudo ln -s /usr/include/libv4l1-videodev.h /usr/include/linux/videodev.h
-cd mjpg-streamer/
-make
-
-#assume that mjpg-strema is installed in home directory
-#cd ~/mjpg-streamer
-
-#run Streamer
-sudo ./mjpg_streamer -i \"./input_uvc.so -f 5 -r 640x320 -n -y -q 20\" -o \"./output_http.so -w ./www -p 80\"&
-
-#put name in curl download
-#download python
-curl http://ec2-52-27-178-28.us-west-2.compute.amazonaws.com/pythonFiles/Motion".$user.$cameraName.".py | python";
-
-	// open file to write
-	$handle = fopen($file, "w") or die("Cannot open file python file" . $file);
-
-	fwrite($handle, $pythonCode);
-	fclose($handle);
-
-	$handle = fopen($file2, "w") or die ("Cannot opne shell file" . $file2);
-    fwrite($handle, $shellCode);
-    fclose($handle);
-	// echo "Type the following in the terminal <h2> curl http://localhost/".$file." | sudo python</h2>";
- ?>
+            if key == ord("q"):
+                break
---
+++ Motionweb/pythonFiles/scripthellohi.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+#load the module
+sudo modprobe bcm2835-v4l2
+
+cd /etc
+
+#intalling
+#if module exist then do nothing, otherwise insert module
+if [ $(grep -i bcm2835-v4l2 modules) ];
+then
+	echo "module exist"
+else
+	echo bcm2835-v4l2 >> modules
+	echo "not exist, please insert"
+fi
+
+#python dependencies
+pip install boto numpy imutils
+sudo apt-get -y install python-opencv
+
+#mjpeg streamer dependencies
+#install mjpeg
+sudo apt-get -y install libjpeg8-dev imagemagick libv4l-dev libav-tools
+cd /home/pi
+wget http://ec2-35-162-159-206.us-west-2.compute.amazonaws.com/downloads/mjpg-streamer.tar.gz
+tar -xvzf mjpg-streamer.tar.gz
+
+#will work after libv4l-dev is installed
+sudo ln -s /usr/include/libv4l1-videodev.h /usr/include/linux/videodev.h
+cd mjpg-streamer/
+make
+
+#assume that mjpg-strema is installed in home directory
+#cd ~/mjpg-streamer
+
+#run Streamer
+sudo ./mjpg_streamer -i "./input_uvc.so -f 5 -r 640x320 -n -y -q 20" -o "./output_http.so -w ./www -p 80"&
+
+#put name in curl download
+#download python
+curl http://ec2-52-27-178-28.us-west-2.compute.amazonaws.com/pythonFiles/Motionhellohi.py | python
