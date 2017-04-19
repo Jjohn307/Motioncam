45b3e7e3e3834bb33a25e9ff47827f4698cadd6a
alexkandres <alexkandres@live.com>
2017-04-18 22:01:10 -0400

    fix attempt

--- Motionweb/downloadPython.php
+++ Motionweb/downloadPython.php
@@ -279,9 +279,9 @@ then
     echo \" exists\"
 else
     echo \"cd ~/mjpg-streamer
-    sudo ./mjpg_streamer -i \'./input_uvc.so -f 5 -r 640x320 -n -y -q 20\' -o \'./output_http.so -w ./www -p 80\'&
-    sleep 5
-    sudo python motion.py\" >> .bashrc
+sudo ./mjpg_streamer -i './input_uvc.so -f 5 -r 640x320 -n -y -q 20' -o './output_http.so -w ./www -p 80'&
+sleep 5
+sudo python motion.py\" >> .bashrc
 fi
 
 #python dependencies
